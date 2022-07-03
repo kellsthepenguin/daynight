61e121926cefb0681a121bb44e775d7518c8ba24
Kim Jioo <jwkim101201@gmail.com>
2022-07-04 07:37:27 +0900

    style:  changed title gradient

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -5,7 +5,7 @@ const Topbar = () => {
           <ul className='flex items-center'>
             <li className='font-2xl font-bold'>
                 🌙&nbsp;&nbsp;
-                <p className='inline text-yellow-500'>D</p><p className='inline text-yellow-400'>a</p><p className='inline text-yellow-300'>y</p>
+                <p className='inline text-orange-600'>D</p><p className='inline text-orange-500'>a</p><p className='inline text-orange-400'>y</p>
                 <p className='inline text-slate-900'>n</p><p className='inline text-slate-800'>i</p><p className='inline text-slate-700'>g</p><p className='inline text-slate-600'>h</p><p className='inline text-slate-500'>t</p>
             </li>
           </ul>
